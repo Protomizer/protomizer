dd329af3f40c05b7c6c713c7cc795a33433ff76e
Protomizer <133832204+Protomizer@users.noreply.github.com>
2023-06-04 22:16:36 -0400

    Delete Loading.js

--- Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loading = () => {
-  return (
-    <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-  )
-}
-
-export default Loading
